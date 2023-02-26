1af6c8ac755cb60d399041d8e6ecf0bf6bae3303
Владислав Максаев <90024139+vladNEKvodan@users.noreply.github.com>
2023-02-27 01:05:23 +0300

    Add files via upload

---
+++ demo8/src/main/java/com/example/demo8/Model.java
@@ -0,0 +1,18 @@
+package com.example.demo8;
+
+public class Model {
+    public long calculation(long a, long b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b == 0) return 0;
+                return a / b;
+        }
+        return 0;
+    }
+}
